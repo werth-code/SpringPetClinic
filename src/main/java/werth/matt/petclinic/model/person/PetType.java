71a33dc628c73ce3e20186b51aea6cf98a18a266
Matt Werth <matthewwerth@gmail.com>
2021-02-01 08:24:58 -0500

    Added Classes

---
+++ src/main/java/werth/matt/petclinic/model/person/PetType.java
@@ -0,0 +1,13 @@
+package werth.matt.petclinic.model.person;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/werth/matt/petclinic/model/person/Vet.java
@@ -0,0 +1,4 @@
+package werth.matt.petclinic.model.person;
+
+public class Vet extends Person {
+}
